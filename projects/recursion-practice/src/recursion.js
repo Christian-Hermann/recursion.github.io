b29667fcdbf4fe2fd1df9e1dc7ce0d2674ffc54e
Christian-Hermann <christianghermann@gmail.com>
2024-11-19 15:36:56 -0600

    4

--- projects/recursion-practice/src/recursion.js
+++ projects/recursion-practice/src/recursion.js
@@ -38,10 +38,22 @@ return array[0] + sum(array.slice(1));
 // 3. Sum all numbers in an array containing nested arrays.
 // Example: arraySum([1,[2,3],[[4]],5]); // 15
 var arraySum = function(array) {
+
 };
 
 // 4. Check if a number is even.
 var isEven = function(n) {
+
+n = Math.abs(n);
+
+if (n === 0){
+  return true;
+} else if ( n === 1) {
+  return false;
+}
+ 
+return isEven(n - 2);
+
 };
 
 // 5. Sum all integers below a given integer.
